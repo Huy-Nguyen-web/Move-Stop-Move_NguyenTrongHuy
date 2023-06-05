5784605d6ca193d6e7304f2e1c14f865a1b7a9b8
Huy Nguyen <77337321+Huy-Nguyen-web@users.noreply.github.com>
2023-06-05 20:16:55 +0700

    (~) Fix check obstacle collider

--- Assets/Game/Scripts/CheckObstacleCollider.cs
+++ Assets/Game/Scripts/CheckObstacleCollider.cs
@@ -18,17 +18,9 @@ public class CheckObstacleCollider : MonoBehaviour
     }
     private void Update() {
         transform.position = player.transform.position;
-        // Collider[] obstacles = Physics.OverlapSphere(transform.position, 6f, obstacleLayer);
-        // if(obstacles.Length > 0){
-        //     for (int i = 0; i < obstacles.Length; i++){
-        //         Debug.Log(obstacles[i].transform.GetComponent<MeshRenderer>().material);
-        //         obstacles[i].transform.GetComponent<MeshRenderer>().material = fadeObstacleMaterial;
-        //     }
-        // }
         float offsetY = 100f;
         Vector3 screenCenter = new Vector3(Screen.width * 0.5f, Screen.height * 0.5f - offsetY, 0f);
         Ray ray =  Camera.main.ScreenPointToRay(screenCenter);
-
         RaycastHit hit;
         if(Physics.Raycast(ray, out hit, Mathf.Infinity, obstacleLayer)){
             if(!hitObstacle){
